84a7b710d8c295edab2bf0e26ed054e8c3fefdd8
Apethesis <apethesis@gmail.com>
2024-09-16 18:45:04 +0200

    Add data object and sequelize definitions.

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
 require('dotenv').config();
 const fs = require('node:fs');
 const path = require('node:path');
+const { Sequelize, DataTypes } = require('sequelize');
 const { Client, Collection, Events, GatewayIntentBits, REST, Routes } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] }); 
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'db.sqlite'
+})
 client.commands = new Collection();
 const cmdRegister = []
 const rest = new REST().setToken(process.env.DTOK);
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const data = {
+    titles: [],
+    textEntry: sequelize.define('textEntry',{
+        id: {
+            primaryKey: true,
+            allowNull: false,
+            type: DataTypes.BIGINT,
+            unique: true,
+        },
+        content: {
+            allowNull: false,
+            type: DataTypes.TEXT
+        },
+        lastEditor: DataTypes.STRING,
+        creator: DataTypes.STRING
+    }),
+    userInfo: sequelize.define('userInfo', {
+        id: {
+            primaryKey: true,
+            allowNull: false,
+            type: DataTypes.BIGINT,
+            unique: true,
+        },
+        access: {
+            allowNull: false,
+            defaultValue: 0,
+            type: DataTypes.INTEGER,
+            validate: {
+                max: 5,
+                min: 0
+            }
+        }
+    })
+}
+
+if (fs.existsSync("titles.json")) {
+    data.titles = Object.values(JSON.parse(fs.readFileSync("titles.json")))
+}
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
@@ -29,7 +72,16 @@ rest.put(
     console.log(err)
 })
 
-client.on(Events.InteractionCreate, async interaction => {
+client.once(Events.ClientReady, (cl) => {
+    sequelize.sync({ alter: true }).then(() => {
+        console.log("Synchronized.")
+    }).catch((err) => {
+        console.log(err)
+    })
+    console.log("Ready!")
+})
+
+client.on(Events.InteractionCreate, (interaction) => {
 	if (!interaction.isChatInputCommand()) return;
     const command = interaction.client.commands.get(interaction.commandName);
 
